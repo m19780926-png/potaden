79074331ef8003197452b2281b0c5dfd01949587
m19780926-png <m19780926@gmail.com>
2026-06-21 17:45:37 +0900

    Add service worker for caching files
    Implement service worker for caching and offline support.

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "pota-den-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// インストール
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// オフライン対応
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
